5f10c70dcc15780f2e33b68ca44902eadde38cde
LAPTOP-1BHIFIN7\LENOVO <duonggiaminh2003@gmail.com>
2023-10-24 17:08:07 +0700

    update ex8

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -146,7 +146,7 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   setTimer1(100);
-  setTimer2(100);
+  setTimer2(25);
   int hour = 15, minute = 8, second = 50;
   while (1)
   {
@@ -168,7 +168,7 @@ int main(void)
 		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
 	  }
 	  if(timer2_flag == 1){
-		  setTimer2(100);
+		  setTimer2(25);
 		  update7SEG(index_led);
 		  index_led = (index_led + 1) % MAX_LED;
 	  }
