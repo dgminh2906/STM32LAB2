386963d3df8a66d1e2e7f3b6ebca1b0e25745a86
LAPTOP-1BHIFIN7\LENOVO <duonggiaminh2003@gmail.com>
2023-10-25 11:56:51 +0700

    update led red

--- LAB2/Core/Src/main.c
+++ LAB2/Core/Src/main.c
@@ -166,6 +166,7 @@ int main(void)
 		  }
 		  updateClockBuffer(hour, minute);
 		  HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+		  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 	  }
 	  if(timer2_flag == 1){
 		  setTimer2(25);
